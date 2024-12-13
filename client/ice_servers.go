06d8a98a66a84aee8ef7977f1977b2c02fffa459
Oyedele Temitope Taiwo <51465664+oyedeletemitope@users.noreply.github.com>
2024-12-13 09:19:24 -0800

    Update ice_servers.go

--- client/ice_servers.go
+++ client/ice_servers.go
@@ -2,7 +2,6 @@ package client
 
 import "github.com/oyedeletemitope/simli-go-sdk/models"
 
-// GetICEServers fetches ICE server configurations for WebRTC connections.
 func (c *Client) GetICEServers(request models.ICERequest) (*models.ICEResponse, error) {
 	var response models.ICEResponse
 	err := c.post("/getIceServers", request, &response)
