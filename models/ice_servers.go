1ca48d5b99fd79bb43749f65da23cbd46bfcff1a
Oyedele Temitope Taiwo <51465664+oyedeletemitope@users.noreply.github.com>
2024-12-20 05:58:47 -0800

    Update ice_servers.go

--- models/ice_servers.go
+++ models/ice_servers.go
@@ -7,3 +7,9 @@ type ICERequest struct {
 type ICEResponse struct {
 	URLs []string `json:"urls"`
 }
+
+type IceServer struct {
+	URLs       []string `json:"urls"`
+	Username   string   `json:"username,omitempty"`
+	Credential string   `json:"credential,omitempty"`
+}
